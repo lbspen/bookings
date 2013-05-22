a04d7921ebbec7d959fce9847e5a75bfddcedc72
lbs <lbspen@gmail.com>
2013-05-22 13:23:46 -0700

    Initial app generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookings::Application.config.secret_token = '4ac892a34da8c0b624dfaf941ce5b5289c7a86951e99408c95b30cf512990b0fb0e7556e3b09a1efe243d8f3e1e2ebac68bc660090cad9dc351a1bd78e807643'
