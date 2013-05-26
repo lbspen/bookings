934f6430ad8352066145d8db817c343c51a379d8
lbs <lbspen@gmail.com>
2013-05-26 16:11:26 -0700

    client has many engineers through bookings

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,6 @@
 class Client < ActiveRecord::Base
   attr_accessible :name
+  has_many :bookings
+  has_many :engineers, :through => :bookings
   validates :name, :presence => true
 end
--- spec/integration/viewing_clients_spec.rb
+++ spec/integration/viewing_clients_spec.rb
@@ -1,14 +1,20 @@
 require 'spec_helper'
 
 feature "Viewing clients" do
+  let!(:client) { Factory.create(:client, :name => "Joe") }
   scenario "Listing all clients" do
-    client = Factory.create(:client, :name => "Joe")
     visit clients_url
     click_link client.name
     page.current_url.should == client_url(client)
   end
 
-  scenario "Viewing all engineers" do
-    pending "View unordered list of engineers"
+  scenario "Viewing engineers associated with a client" do
+    booking1 = Factory.create(:booking, :client => client)
+    engineer = Factory.create(:engineer, :name => "Sam" )
+    booking2 = Factory.create(:booking, :client => client, :engineer => engineer)
+    visit clients_url
+    click_link client.name
+    page.should have_content(booking1.engineer.name)    
+    page.should have_content(booking2.engineer.name)
   end
 end
